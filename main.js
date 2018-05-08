f1bd8e254952d7c1b54d24f6081a6f39a25a6f49
tinybarks <31551149+tinybarks@users.noreply.github.com>
2018-05-08 10:59:19 +0200

    License?

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+/**
+ * botbscroller - https://github.com/tinybarks/botbscroller
+ * To the extent possible under law, the person who associated CC0 with
+ * this project has waived all copyright and related or neighboring rights
+ * to this project.
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+
 // shamelessly cobbled together from examples
 
 document.onreadystatechange = () => {
