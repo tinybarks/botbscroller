ba35c6da8e6601d827e2468f65a8d3f813a94b62
tinybarks <31551149+tinybarks@users.noreply.github.com>
2018-05-08 10:50:29 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,62 @@
+// shamelessly cobbled together from examples
+
+document.onreadystatechange = () => {
+    if (document.readyState === 'complete') {
+        ready();
+    }
+};
+
+function ready() {
+    const app = new PIXI.Application();
+    document.body.appendChild(app.view);
+
+    const texture = PIXI.Texture.fromImage("botb_bg.png");
+    const tilingSprite = new PIXI.extras.TilingSprite(
+        texture,
+        app.screen.width,
+        app.screen.height
+    );
+
+    let colorMatrix = new PIXI.filters.ColorMatrixFilter();
+    app.stage.filters = [colorMatrix];
+
+    // default matrix:
+    // colorMatrix.matrix = [
+    //     1, 0, 0, 0, 0,      // r
+    //     0, 1, 0, 0, 0,      // g
+    //     0, 0, 1, 0, 0,      // b
+    //     0, 0, 0, 1, 0];     // a
+
+    // mess around with the logo colors, you could make this dynamic
+    colorMatrix.matrix = [
+        2.5,  0,   0,   0,   1,      // r
+        0,    0.1, 0,   0,   0,      // g
+        0,    0,   0.1, 0,   0,      // b
+        0,    0,   0,   1,   0];     // a
+
+    // resize/scaling code to maintain size and aspect ratio
+    const onResize = () => {
+        const w = window.innerWidth;
+        const h = window.innerHeight;
+        app.renderer.view.style.width = w + "px";
+        app.renderer.view.style.height = h + "px";
+        app.renderer.resize(w, h);
+        // app.screen.width = w;
+        // app.screen.height = h;
+        tilingSprite.width = w;
+        tilingSprite.height = h;
+    };
+
+    window.onresize = onResize;
+    onResize();
+
+    app.stage.addChild(tilingSprite);
+
+    // main gfx loop
+    app.ticker.add(() => {
+        tilingSprite.tileScale.x = 2;
+        tilingSprite.tileScale.y = 2;
+        tilingSprite.tilePosition.x += 1;
+        tilingSprite.tilePosition.y += 1;
+    });
+}
